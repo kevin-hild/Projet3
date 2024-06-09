0a4aaa0f6d4103a3c68c85e46f627da70e0359c4
Kevin Hild <kevin.j.hild@gmail.com>
2024-06-09 14:07:17 +0200

    ajoute du filtre

--- FrontEnd/travaux.js
+++ FrontEnd/travaux.js
@@ -25,3 +25,25 @@ for (let i = 0; i < works.length; i++) {
 }
 getWorks()
 
+async function getCategories() {
+  const reponse = await fetch("http://localhost:5678/api/categories")
+  const categories = await reponse.json();
+  console.log(categories)
+
+  
+  for(let i = 0; i < categories.length; i++) {
+    const categorie = categories[i];
+
+    const sectionElement = document.querySelector(".categories");
+
+    const btnElement = document.createElement("button");
+    btnElement.classList.add("bouton_filtre");
+    btnElement.innerText = categorie.name;
+
+    sectionElement.appendChild(btnElement);
+  
+  }
+
+}
+getCategories()
+
