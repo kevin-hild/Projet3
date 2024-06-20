d2c7e2af8274325c8f92a2e7a38c5802d924dc2f
Kevin Hild <kevin.j.hild@gmail.com>
2024-06-20 14:47:21 +0200

    simplification du code login.js

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -1,41 +1,37 @@
 
-document.addEventListener('DOMContentLoaded', () => {
+// document.addEventListener('DOMContentLoaded', () => {
     
-    document.getElementById('loginForm').addEventListener('submit', function(event) {
-        event.preventDefault();
+//     document.getElementById('loginForm').addEventListener('submit', checkLogin); 
+// });
 
-        const email = document.getElementById('email').value;
-        const password = document.getElementById('password').value;
+document.getElementById('loginForm').addEventListener('submit', checkLogin); 
 
-        console.log('Email:', email);
-        console.log('Password:', password);
+async function checkLogin(event) {
+    event.preventDefault();
 
-        fetch('http://localhost:5678/api/users/login', {
-            method: 'POST',
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify({ email: email, password: password })
-        })
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
 
-        .then(async response => {
-            if (!response.ok) { 
-        }
-            return response.json();
-        })
-        
-        .then(data => {
-            if (data.token) {
+    // console.log('Email:', email);
+    // console.log('Password:', password);
+
+
+    const response = await fetch('http://localhost:5678/api/users/login', {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({ email: email, password: password })
+    });
+    const data =  await response.json();
+    
+        if (data.token) {
             // Stocker le token dans le localStorage
             localStorage.setItem('token', data.token);
             // Rediriger vers la page d'accueil en cas de succès
             window.location.href = 'index.html';
-        } else {
+        }  
             // Afficher un message d'erreur en cas d'échec de la connexion
             const errorMessage = document.getElementById("error-message");
-            errorMessage.innerText = 'Mauvaise Email ou mot de passe';
-            errorMessage.style.opacity = 1;
-            }
-        })
-    });
-});
-
+            errorMessage.innerText = 'Mauvais Email ou mot de passe';
+            errorMessage.style.opacity = 1;    
+};
 
